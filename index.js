e9cf41c847365cf6f498e2b8fa1925a24af4f115
martin-XPS-15-9510 <oowuoroowuor@gmail.com>
2024-03-28 01:15:39 +0300

    update function continued lab js file

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // code your solution here
+
+function saturdayFun (string = "roller-skate"){
+
+    return `This Saturday, I want to ${string}!`;
+
+}
+saturdayFun ('bathe my dog!');
+
+
+
+mondayWork = (activiTY = 'go to the office') => {
+
+    return `This Monday, I will ${activiTY}.`;
+}
+mondayWork ("work from home");
+
+
+
+function wrapAdjective (name1){
+    
+    return function mySpecial (name = "special") {
+        return `You are ${name1}${name}${name1}!`
+        
+    }
+    
+    
+    
+}
+ console.log(wrapAdjective("*")("a hard worker")); //=> "You are %a dedicated programmer%!"
